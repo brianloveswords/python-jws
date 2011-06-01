df29c1a4d633699183693569cec0ddb84463c711
Brian J Brennan <brianloveswords@gmail.com>
2011-06-01 04:02:51 -0400

    Comments + refactoring some variable names.

--- jwt/jwk.py
+++ jwt/jwk.py
@@ -5,20 +5,24 @@ class AlgorithmError(Exception): pass
 
 class JWK(object):
     @classmethod
-    def to_real_key(klass, webkey_entry):
-        return getattr(klass, 'to_%s' % webkey_entry['algorithm'])(webkey_entry)
+    def to_real_key(klass, webkey):
+        return getattr(klass, 'to_%s' % webkey['algorithm'])(webkey)
     
     @classmethod
-    def to_ECDSA(klass, webkey_entry):
+    def to_ECDSA(klass, webkey):
         import ecdsa
         curves = {
             'P-256': ecdsa.NIST256p,
             'P-384': ecdsa.NIST384p,
             'P-521': ecdsa.NIST521p,
         }
-        x = long(b64decode(webkey_entry['x']))
-        y = long(b64decode(webkey_entry['y']))
-        curve = curves[webkey_entry['curve']]
+        
+        x = long(b64decode(webkey['x']))
+        y = long(b64decode(webkey['y']))
+        curve = curves[webkey['curve']]
+        
+        # from_public_point() takes an ellipticcurve.Point instance, which must
+        # be generated from a CurveFp instance, long x, and long y.
         point = ecdsa.ellipticcurve.Point(curve.curve, x, y)
         return ecdsa.VerifyingKey.from_public_point(point, curve)
         
@@ -27,10 +31,9 @@ class JWK(object):
         from ecdsa.keys import VerifyingKey as ECDSAKey
         from Crypto.PublicKey.RSA import _RSAobj as RSAKey
         
-        # keyed by real class
-        known_types = {
-            ECDSAKey: 'ECDSA', RSAKey: 'RSA',
-        }
+        # keyed by actual class. will use value to generate a method call
+        known_types = { ECDSAKey: 'ECDSA', RSAKey: 'RSA', }
+        
         try:
             keytype = known_types[keyobj.__class__]
         except KeyError, e:
@@ -41,7 +44,6 @@ class JWK(object):
     def from_ECDSA(klass, keyobj):
         point = keyobj.pubkey.point
         curve = 'P-%s' % keyobj.curve.name[4:7] # e.g. NIST256p, we only want the 256
-        x, y = point.x(), point.y()
         return {
             'algorithm': 'ECDSA',
             'curve': curve,
