67c24976a2db20023c240e303fbf253f16a43c95
Brian J Brennan <brianloveswords@gmail.com>
2013-08-07 17:32:53 -0700

    Change name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "python-jws",
+    name = "jws",
     version = "0.1.0",
     author = "Brian J Brennan",
     author_email = "brian@nyhacker.org",
