40d979b0769ee4f6fcc6155ae45cae649f6da901
Thomas Grainger <tom.grainger@procensus.com>
2017-01-11 10:34:02 +0000

    PyPI now rejects register calls
    upload is now register/upload

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@ from fabric.api import local
 
 # Automate the release
 def release():
-    local("python setup.py sdist register upload")
-    local("python2.5 setup.py bdist_egg register upload")
-    local("python2.6 setup.py bdist_egg register upload")
-    local("python2.7 setup.py bdist_egg register upload")
+    local("python setup.py sdist upload")
+    local("python2.5 setup.py bdist_egg upload")
+    local("python2.6 setup.py bdist_egg upload")
+    local("python2.7 setup.py bdist_egg upload")
