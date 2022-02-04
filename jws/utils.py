5123781058b647ed7fa0762f50823d76cab69831
Aleksey <creater777@yandex.ru>
2022-02-04 18:23:26 +0300

    fix verify function

--- jws/utils.py
+++ jws/utils.py
@@ -47,5 +47,5 @@ def constant_time_compare(val1, val2):
         return False
     result = 0
     for x, y in zip(val1, val2):
-        result |= ord(x) ^ ord(y)
+        result |= x ^ y
     return result == 0
